3e83f42ae5ca9e96b4e2b31f23b7a1b93d8005a1
mattc <matthew.cocar@eservicecorp.ca>
2022-09-16 14:40:25 -0700

    run formatting

--- config/nvim/extra.lua
+++ config/nvim/extra.lua
@@ -9,7 +9,7 @@ end
 
 require("mason").setup()
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
   snippet = {
@@ -50,11 +50,11 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-    }
-    -- {{ name = 'buffer' },}
-    )
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+  }
+  -- {{ name = 'buffer' },}
+  )
 })
 
 -- Set configuration for specific filetype.
@@ -63,7 +63,7 @@ cmp.setup.filetype('gitcommit', {
   --    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
   --  }
   sources = cmp.config.sources({
-      { name = 'buffer' },
+    { name = 'buffer' },
   })
 })
 
@@ -79,15 +79,15 @@ cmp.setup.cmdline('/', {
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
 })
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
@@ -111,7 +111,8 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-t>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
@@ -135,25 +136,40 @@ mason_lspconfig.setup({
 
 local servers = { "pyright", "rust_analyzer", "tsserver", "solargraph", "yamlls", "sumneko_lua", "ansiblels" }
 for _, lsp in pairs(servers) do
- lspconfig[lsp].setup {
-   on_attach = on_attach,
-   capabilities = capabilities,
-   flags = {
-     -- This will be the default in neovim 0.7+
-     debounce_text_changes = 150,
-   }
- }
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
 end
 
-lspconfig.yamlls.setup {
- on_attach = on_attach,
- capabilities = capabilities,
- settings = {
-   yaml = {
-     schemas = {
-       ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json"] = { "*-tmpl.yaml", "*-tmpl.yml" },
-     },
-   },
- }
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      telemetry = {
+        enable = false
+      },
+      format = {
+        enable = true
+      }
+    }
+  }
 }
 
+lspconfig.yamlls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json"] = { "*-tmpl.yaml",
+          "*-tmpl.yml" },
+      },
+    },
+  }
+}
